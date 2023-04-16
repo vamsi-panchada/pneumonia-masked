5e850ffcc485a085156ed255cce0b78c958f36b3
vamsi panchada <vamsivs58@gmail.com>
2023-04-16 11:14:59 +0530

    new Feature Added

--- app.py
+++ app.py
@@ -175,6 +175,12 @@ uploadedFiles = st.file_uploader('Upload Chest X-Rays', accept_multiple_files=Tr
 
 imageArray = []
 betaColumnArray = []
+midColumnArray = []
+
+maskCheck = st.checkbox('Display Masks')
+
+if maskCheck:
+    maskRadio = st.radio('which you want to display', ['mask', 'segmented'])
 
 if uploadedFiles:
     st.markdown("""<hr style="height:10px;border:none;color:#333;background-color:#333;" /> """, unsafe_allow_html=True)
@@ -182,21 +188,27 @@ if uploadedFiles:
 
         file_bytes = np.asarray(bytearray(upload_file.read()), dtype=np.uint8)
         im = cv2.imdecode(file_bytes, 1)
-        col1, mid, col2 = st.columns([10, 1, 20])
+        col1, mid, col2 = st.columns([20, 20, 20])
         col1.image(cv2.resize(im, (224, 224), interpolation=cv2.INTER_CUBIC), channels="BGR")
         col1.write(upload_file.name)
         imageArray.append(im)
         betaColumnArray.append(col2)
+        midColumnArray.append(mid)
         st.markdown("""<hr style="height:10px;border:none;color:#333;background-color:#333;" /> """, unsafe_allow_html=True)
 
 if len(imageArray)>0:
     if st.button('Predict'):
-        for im, col2 in zip(imageArray, betaColumnArray):
+        for im, mid, col2 in zip(imageArray, midColumnArray, betaColumnArray):
             im = cv2.resize(im, (512, 512))[:,:,0]
             im = im.reshape(1, 512, 512, 1)
             mask = model.predict(im).reshape(512, 512)
             im = im.reshape(512, 512)
             im[mask==0]=0
+            if maskCheck:
+                if maskRadio == 'mask':
+                    mid.image(mask, channels='GRAY', caption='Generated Mask')
+                elif maskRadio == 'segmented':
+                    mid.image(im, channels='GRAY', caption='Segmented Image')
             im = cv2.resize(im, (224, 224), interpolation=cv2.INTER_CUBIC).reshape(1, 224, 224, 1)
             im = im.astype(np.float32)/255.
             
